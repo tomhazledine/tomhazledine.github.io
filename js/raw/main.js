feb672863ab4a6b0074d9c464f4ded13eeabfdcb
Tom Hazledine <tom@eatenbymonsters.com>
2014-01-07 18:55:31 +0000

    tweaks to deal with FOUT

---
+++ js/raw/main.js
@@ -0,0 +1,9 @@
+// Test Alert
+//$(document).ready(function(){alert("Main JS Working");});
+
+// Fight the FOUT!
+$('html').addClass('nofout');
+
+$(document).ready(function() {
+  $('html').show();
+});
