922b9958b6a5da15a264745e3dd9cd6ccc7ac521
Tom Hazledine <tom@eatenbymonsters.com>
2014-03-24 12:39:13 +0000

    pre v4 cleanup

--- _site/js/raw/main.js
+++ _site/js/raw/main.js
@@ -7,3 +7,13 @@ $('html').addClass('nofout');
 $(document).ready(function() {
   $('html').show();
 });
+
+// Drop Cap
+//Grab first character for Drop Caps
+$(".big-drop-cap").each(function() {
+  var text = $(this).html();
+  // RegEx version
+  // $(this).html(text.replace(/^([A-Za-z0-9])/g,'<span class="caps">$1</span>'));
+  var first = $('<span>'+text.charAt(0)+'</span>').addClass('dropcap');
+  $(this).html(text.substring(1)).prepend(first);
+});
--- js/raw/main.js
+++ js/raw/main.js
@@ -7,3 +7,13 @@ $('html').addClass('nofout');
 $(document).ready(function() {
   $('html').show();
 });
+
+// Drop Cap
+$(".big-drop-cap").each(function() {
+  var text = $(this).html();
+  // RegEx method: $(this).html(text.replace(/^([A-Za-z0-9])/g,'<span class="caps">$1</span>'));
+  
+  var first = $('<span>'+text.charAt(0)+'</span>').addClass('dropcap');// Span the 1st character
+  var second = $('<span>'+text.charAt(1)+'</span>').addClass('dropcap-run-in');// Span the 2nd character
+  $(this).html(text.substring(2)).prepend(first, second);// replace 1st and 2nd chars with new spaned versions
+});
