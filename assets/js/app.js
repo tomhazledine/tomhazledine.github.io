c4ca65886215846e10999a1d69cadc7a6a917615
DWM <dwm@Marks-MacBook-Air-272.local>
2015-06-08 07:47:29 +0100

    copied over calc code from CodePen

--- assets/js/app.js
+++ assets/js/app.js
@@ -10641,6 +10641,159 @@ mainMenuToggle.on('click',function(){
         header.addClass('mainMenuOpen');
     }
 });
+//var MegaCalcuWower = (function(output,buttons){
+
+var calcWrap = $('#calcWrap');
+
+if (calcWrap.length) {
+
+    var buttons = calcWrap.find('.buttons');
+    var output = calcWrap.find('.output');
+
+    var calc = new MegaCalcuWower(buttons,output);
+
+    function MegaCalcuWower(buttons, output){
+        
+
+        // Utilities
+        var hasDecimal = false;
+        var memory = [];
+        var currentOperator;
+        var operators = ['-','+','*'];
+        
+        // Elements
+        var numbers = buttons.find('.number');
+        var clear = buttons.find('.clear');
+        var decimal = buttons.find('.decimal');
+        var plus = buttons.find('.plus');
+        var equals = buttons.find('.equals');
+        var minus = buttons.find('.minus');
+        var times = buttons.find('.times');
+        
+        function clearOutput(x){
+            output.text(x);
+            return hasDecimal = false;
+        }
+
+        function error(){
+            output.text('Err');
+            return hasDecimal = false;
+        }
+
+        function checkLength(i){
+            var countResult = i.toString().length;
+            if (countResult > 12) {
+                error();
+                return false;
+            }
+        }
+
+        function saveValue(i){
+            currentOutput = output.text();
+            memory[i] = currentOutput;
+            return memory;
+        }
+        
+        // Do the maths
+        function operate(term1,term2,operator){
+            var result = '0';
+            if (operator == '+'){
+                var term1 = parseFloat(memory[0]);
+                var term2 = parseFloat(currentOutput);
+                result = term1 + term2;
+                return result;
+            } else if (operator == '-'){
+                var term1 = parseFloat(memory[0]);
+                var term2 = parseFloat(currentOutput);
+                result = term1 - term2;
+                return result;
+            } else if (operator == '*'){
+                var term1 = parseFloat(memory[0]);
+                var term2 = parseFloat(currentOutput);
+                result = term1 * term2;
+                return result;
+            }
+        }
+        
+        // Listeners
+        numbers.on('click', function(){
+            clickedNumber = $(this).data('num');
+            currentOutput = output.text();
+            if ( isNaN(currentOutput) || currentOutput == '0') {
+                currentOutput = '';
+            }
+            newOutput = currentOutput + clickedNumber;
+            if (checkLength(newOutput) == false){
+                return;
+            }
+            output.text(newOutput);
+        });
+        
+        decimal.on('click', function(){
+            if (hasDecimal == false) {
+                clickedNumber = '.';
+                currentOutput = output.text();
+                if ( isNaN(currentOutput)) {
+                    //if (isInArray(currentOutput,operators)) {
+                        //currentOutput = '0';
+                    //} else {
+                        currentOutput = '0';
+                    //}
+                }
+                newOutput = currentOutput + clickedNumber;
+                if (checkLength(newOutput) == false){
+                    return;
+                }
+                output.text(newOutput);
+                return hasDecimal = true;
+            }
+        });
+
+        clear.on('click', function(){
+            clearOutput('0');
+        });
+
+        plus.on('click', function(){
+            saveValue(0);
+            currentOperator = '+';
+            clearOutput('+');
+        });
+
+        minus.on('click', function(){
+            saveValue(0);
+            currentOperator = '-';
+            clearOutput('-');
+        });
+
+        times.on('click', function(){
+            saveValue(0);
+            currentOperator = '*';
+            clearOutput('*');
+        });
+        
+        equals.on('click', function(){
+            currentOutput = output.text();
+            var result = operate(memory[0],currentOutput,currentOperator);
+            
+            
+            var countResult = result.toString().length;
+            if (checkLength(result) == false){
+                return;
+            }
+            output.text(result);
+            return hasDecimal = false;
+        });
+    }
+    //})();
+
+    //var buttons = $('.buttons');
+    //var output = $('.output');
+
+    //var calulator = new MegaCalcuWower(output, buttons);
+
+
+
+}
 var nextPrevWrapper = $('#nextPrevWrapper');
 
 if (nextPrevWrapper.length) {
---
+++ calc/index.md
@@ -0,0 +1,42 @@
+---
+layout: bare
+title: "CSS and Javascript Calculator"
+excerpt: "A working calculator built using JS and styled with CSS."
+customCss: "calc.min.css"
+---
+
+<div id="calcWrap">
+
+    <div class="note">
+        Not finished yet... (last update 2015.05.19)
+    </div>
+    <div class="wrapper">
+        <div class="output">0</div>
+        
+        <div class="buttons">
+
+            <div class="numbers">
+                <button class="number" data-num="9">9</button>
+                <button class="number" data-num="8">8</button>
+                <button class="number" data-num="7">7</button>
+                <button class="number" data-num="6">6</button>
+                <button class="number" data-num="5">5</button>
+                <button class="number" data-num="4">4</button>
+                <button class="number" data-num="3">3</button>
+                <button class="number" data-num="2">2</button>
+                <button class="number" data-num="1">1</button>
+                <button class="clear">C</button>
+                <button class="number number0" data-num="0">0</button>
+                <button class="decimal" data-num=".">.</button>
+            </div>
+
+            <div class="operators">
+                <button class="plus">+</button>
+                <button class="minus">-</button>
+                <button class="times">&times;</button>
+                <button class="equals">=</button>
+            </div>
+            
+        </div>
+    </div>
+</div>
