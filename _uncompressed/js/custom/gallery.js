e02df3c11824eb536481be8a844448990e03e45a
Tom Hazledine <tom@eatenbymonsters.com>
2015-07-30 13:17:47 +0100

    keybindings for image gallery

--- _uncompressed/js/custom/gallery.js
+++ _uncompressed/js/custom/gallery.js
@@ -1,11 +1,37 @@
-var galleryToggles = $('.thumbnailMore');
+var galleryToggles = $('.thumbnailImage');
 var gallery = $('.fullscreenWrapper');
 var galleryClose = $('.fullscreenClose');
+var galleryWrapper = $('#gallery');
 
-galleryToggles.on('click',function openFullscreen(){
-    gallery.addClass('open');
-});
+if (galleryToggles.length) {
+    galleryWrapper.slick({
+        arrows:true,
+        prevArrow: $('.nextPrev.prev'),
+        nextArrow: $('.nextPrev.next')
+    });
 
-galleryClose.on('click',function openFullscreen(){
-    gallery.removeClass('open');
-});
+    galleryToggles.on('click',function openFullscreen(){
+        gallery.addClass('open');
+        var target = this.getAttribute("data-selector");
+        console.log(target);
+        galleryWrapper[0].slick.slickGoTo(parseInt(target));
+    });
+
+    galleryClose.on('click',function openFullscreen(){
+        gallery.removeClass('open');
+    });
+
+    $(document).keyup(function(e){
+        if (gallery.hasClass('open')) {
+            if (e.keyCode == 27) {
+                gallery.removeClass('open');
+            }
+        }
+        // Keybindings for Slick
+        if (e.keyCode == 37) {
+            galleryWrapper[0].slick.slickPrev();
+        } else if (e.keyCode == 39) {
+            galleryWrapper[0].slick.slickNext();
+        }
+    });
+}
